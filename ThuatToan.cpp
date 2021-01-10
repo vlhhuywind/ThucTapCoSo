18ca681b783d957dc95f53d3d697064419f04767
MyLinh60132046 <Linhlinh7953@gmail.com>
2021-01-10 23:26:01 +0700

    lan1

---
+++ ThuatToan.cpp
@@ -0,0 +1,60 @@
+#include<conio.h>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+	int index;  // so thu tu
+	int g;      // khoang cach tu dinh ban dau den dinh hien ta
+	int f;      // f = h + g;
+	int h;      // duong di ngan nhat
+	int color;  // danh dau dinh di qua
+	int parent;    // dinh cha
+};
+
+int a[100][100];
+Node p[100];
+Node Open[100];
+Node Close[100];
+
+void ReadInputFile1(int *b, int &n)
+{
+	fstream fs1("Input1.txt");
+
+	if (!fs1.is_open())
+	{
+		cout << "Khong the mo duoc file!";
+	}
+	else
+	{
+		fs1 >> n;
+
+		for (int i = 0; i < n; i++)
+		{
+			fs1 >> b[i];
+		}
+	}
+    fs1.close();
+}
+
+void ReadInputFile2(int a[100][100], int &n, int &start, int &finsh)
+{
+	fstream fs2("Input2.txt");
+
+	if (!fs2.is_open())
+	{
+		cout << "Khong the mo duoc file!";
+	}
+	else
+	{
+		fs2 >> n >> start >> finsh;
+
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+				fs2 >> a[i][j];
+		}
+	}
+	fs2.close();
+}
